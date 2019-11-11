f87a7a7452913f364f1a505dd76a70926d3e1918
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-11 16:45:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,5 @@
 class Dog
-  def name=(dogs_name)
-      @this_dogs_name = dogs_name
-    end
-
-    def name
-      @this_dogs_name
-    end
-end
+  def initialize(name)
+    @name=name
+  end 
+  end 
